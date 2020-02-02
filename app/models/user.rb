f432adfe36618103c48777ce2ccc24b025387589
miyat-yuka <58365099+miyat-yuka@users.noreply.github.com>
2020-02-02 23:57:21 +0900

    ビューを編集

--- app/models/user.rb
+++ app/models/user.rb
@@ -15,8 +15,7 @@ class User < ApplicationRecord
     # sns認証したことがあればアソシエーションで取得
     # 無ければemailでユーザー検索して取得orビルド(保存はしない)
     user = sns.user || User.where(email: auth.info.email).first_or_initialize(
-      nickname: auth.info.name,
-        email: auth.info.email
+      email: auth.info.email
     )
     # userが登録済みの場合はそのままログインの処理へ行くので、ここでsnsのuser_idを更新しておく
     if user.persisted?
