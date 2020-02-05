fee5a1bc1e4e71f48b57ca21582ae4faa4777a3d
miyat-yuka <58365099+miyat-yuka@users.noreply.github.com>
2020-02-05 18:28:12 +0900

    パラメーター編集

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -34,6 +34,6 @@ class PostsController < ApplicationController
   private
 
     def post_params
-      params.require(:post).permit(:facility_name, :address, :tell, :registration_date, :estimate_sheet, :delivery_date, :delivery_note, :postal_code, :user_id, programs_attributes:[:software, :user, :post_id, hards_attributes:[:category, :program_id, :_destroy, products_attributes:[:thing, :user_id, :hard_id]]]).merge(user_id: current_user.id)
+      params.require(:post).permit(:facility_name, :address, :tell, :registration_date, :estimate_sheet, :delivery_date, :delivery_note, :postal_code, :dealer, :user_id, programs_attributes:[:software, :user, :post_id,  products_attributes:[:thing, :category_id, :model_number, :user_id, :_destroy, :id]]).merge(user_id: current_user.id)
     end
 end
